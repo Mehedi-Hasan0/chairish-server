4073484bcc4ff769b2c3bae6b4bcfb1cbc36545b
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2022-12-05 14:36:36 +0600

    shop api created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
@@ -15,11 +15,27 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const featuredCollection = client.db('chairish').collection('featured');
+        const shopCollection = client.db('chairish').collection('shopProducts')
 
         //getting data for featured section
         app.get('/featured', async (req, res) => {
             const query = {};
             const result = await featuredCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        //api endpoints for shop section
+        //getting api
+        app.get('/shopProducts', async (req, res) => {
+            const query = {};
+            const result = await shopCollection.find(query).toArray();
+            res.send(result);
+        })
+        // get specific products by id
+        app.get('/shopProducts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await shopCollection.findOne(query);
             res.send(result);
         })
     }
